2b23fd65c7f8632a9a488aa05f8cfb223f5cf1df
Jann Jaspher Santos <75710250+thisbejann@users.noreply.github.com>
2023-03-27 12:37:17 +0800

    Changes in calendar

--- app/WeekCalendar.tsx
+++ app/WeekCalendar.tsx
@@ -12,13 +12,56 @@ interface CustomDivProps extends React.HTMLAttributes<HTMLDivElement> {
   rowSpan?: number;
 }
 
-const DAYS_OF_WEEK = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-const TIME_SLOTS = ["8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00"];
+const DAYS_OF_WEEK = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const TIME_SLOTS = [
+  "7:00",
+  "7:30",
+  "8:00",
+  "8:30",
+  "9:00",
+  "9:30",
+  "10:00",
+  "10:30",
+  "11:00",
+  "11:30",
+  "12:00",
+  "12:30",
+  "13:00",
+  "13:30",
+  "14:00",
+  "14:30",
+  "15:00",
+  "15:30",
+  "16:00",
+  "16:30",
+  "17:00",
+  "17:30",
+  "18:00",
+  "18:30",
+  "19:00",
+  "19:30",
+  "20:00",
+  "20:30",
+  "21:00",
+];
 
 export const WeekCalendar: React.FC = () => {
   const schedule: ScheduleItem[] = [
-    { subject: "Math", startTime: "8:00", endTime: "9:00", dayOfWeek: 0 },
-    { subject: "English", startTime: "10:00", endTime: "11:00", dayOfWeek: 1 },
+    { subject: "Embedded Systems", startTime: "9:00", endTime: "11:00", dayOfWeek: 2 },
+    {
+      subject: "Trends in Software Development Process",
+      startTime: "12:00",
+      endTime: "2:00",
+      dayOfWeek: 2,
+    },
+    {
+      subject: "Trends in Software Development Process",
+      startTime: "3:00",
+      endTime: "6:00",
+      dayOfWeek: 2,
+    },
+    { subject: "CPE Practice and Design 2", startTime: "6:00", endTime: "8:00", dayOfWeek: 2 },
+
     { subject: "Science", startTime: "13:00", endTime: "14:00", dayOfWeek: 2 },
     { subject: "History", startTime: "9:00", endTime: "10:00", dayOfWeek: 3 },
     { subject: "PE", startTime: "14:00", endTime: "15:00", dayOfWeek: 4 },
