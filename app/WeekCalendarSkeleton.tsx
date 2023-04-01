e5a70cea9fa777f6e0880e8835e5a6dfed7ae5cf
Joseph De Leon <joeseffdl@gmail.com>
2023-04-01 14:51:23 +0800

    Added Skeleton for WeekCalendarView

---
+++ app/WeekCalendarSkeleton.tsx
@@ -0,0 +1,70 @@
+import { DAYS_OF_WEEK } from "@/utils"
+
+export const WeekCalendarSkeleton = () => (
+  <div className="overflow-hidden rounded-lg shadow-lg">
+    <table className="table-fixed bg-slate-100 p-5 border border-slate-300 shadow-md rounded-lg w-full">
+      <thead>
+        <tr className="border">
+          <th className="w-16 h-10 p-2 border" />
+          <th />
+          <th />
+          <th />
+          <th />
+          <th />
+          <th className="p-2">
+            <button
+              disabled={true}
+              className="disabled:cursor-not-allowed disabled:opacity-50 bg-indigo-700 h-10 text-center text-white text-xs rounded-lg font-semibold px-4 py-2"
+              onClick={() => {}}
+            >
+              Clear Event
+            </button>
+          </th>
+          <th className="p-2">
+            <button
+              disabled={true}
+              className="disabled:cursor-not-allowed disabled:opacity-50 bg-indigo-700 h-10 text-center text-white text-xs rounded-lg font-semibold px-4 py-2"
+              onClick={() => {}}
+            >
+              Save Events
+            </button>
+          </th>
+        </tr>
+        <tr>
+          <th className="w-16"></th>
+          {DAYS_OF_WEEK.map((day) => (
+            <th
+              className="w-1/6 sm:w-auto py-2 text-slate-600 border border-b-2 border-b-slate-300 "
+              key={day}
+            >
+              <span className="font-semibold text-sm">{day.slice(0, 3)}</span>
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {Array(34)
+          .fill(null)
+          .map((_, i) => {
+            const hour = Math.floor(i / 2) + 7
+            const minute = i % 2 === 0 ? "00" : "30"
+            const time = `${hour.toString().padStart(2, "0")}:${minute}`
+
+            return (
+              <tr className="relative select-none h-5 " key={time}>
+                <td className="absolute transform -translate-y-1/2 flex justify-end px-2 items-center text-sm w-16">
+                  {time}
+                </td>
+                {DAYS_OF_WEEK.map((day) => (
+                  <td
+                    key={`${day}-${time}`}
+                    className="h-[2.5rem] border border-gray-200"
+                  />
+                ))}
+              </tr>
+            )
+          })}
+      </tbody>
+    </table>
+  </div>
+)
--- app/WeekCalendarView.tsx
+++ app/WeekCalendarView.tsx
@@ -1,30 +1,29 @@
 "use client";
 
-import { useState, useMemo, useEffect, useCallback } from "react";
-import { scheduleProps } from "@/utils/types";
 import {
   CompareArraysOfObjects,
   DAYS_OF_WEEK,
-  TimeDifference,
   SCHEDULE_DATA_OBJECT,
+  TimeDifference,
 } from "@/utils/";
-import { EventModal } from "./EventModal";
+import { SCHEDULE_ARRAY } from "@/utils/SCHEDULE_ARRAY";
+import { auth, db } from "@/utils/firebase";
+import { scheduleProps } from "@/utils/types";
 import {
   addDoc,
   collection,
-  doc,
-  query,
   limit,
-  orderBy,
   onSnapshot,
-  getDoc,
-  serverTimestamp,
+  orderBy,
+  query,
+  serverTimestamp
 } from "firebase/firestore";
 import { useRouter } from "next/navigation";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth, db } from "@/utils/firebase";
 import { toast } from "react-hot-toast";
-import { SCHEDULE_ARRAY } from "@/utils/SCHEDULE_ARRAY";
+import { EventModal } from "./EventModal";
+import { WeekCalendarSkeleton } from "./WeekCalendarSkeleton";
 
 export const WeekCalendarView = () => {
   const router = useRouter();
@@ -320,76 +319,7 @@ export const WeekCalendarView = () => {
             />
           )}
         </div>
-      ) : (
-        <div className="overflow-hidden rounded-lg shadow-lg">
-          <table className="table-fixed bg-slate-100 p-5 border border-slate-300 shadow-md rounded-lg w-full">
-            <thead>
-              <tr className="border">
-                <th className="w-16 h-10 p-2 border" />
-                <th />
-                <th />
-                <th />
-                <th />
-                <th />
-                <th className="p-2">
-                  <button
-                    disabled={true}
-                    className="disabled:cursor-not-allowed disabled:opacity-50 bg-indigo-700 h-10 text-center text-white text-xs rounded-lg font-semibold px-4 py-2"
-                    onClick={() => {}}
-                  >
-                    Clear Event
-                  </button>
-                </th>
-                <th className="p-2">
-                  <button
-                    disabled={true}
-                    className="disabled:cursor-not-allowed disabled:opacity-50 bg-indigo-700 h-10 text-center text-white text-xs rounded-lg font-semibold px-4 py-2"
-                    onClick={() => {}}
-                  >
-                    Save Events
-                  </button>
-                </th>
-              </tr>
-              <tr>
-                <th className="w-16"></th>
-                {DAYS_OF_WEEK.map((day) => (
-                  <th
-                    className="w-1/6 sm:w-auto py-2 text-slate-600 border border-b-2 border-b-slate-300 "
-                    key={day}
-                  >
-                    <span className="font-semibold text-sm">
-                      {day.slice(0, 3)}
-                    </span>
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {Array(34)
-                .fill(null)
-                .map((_, i) => {
-                  const hour = Math.floor(i / 2) + 7;
-                  const minute = i % 2 === 0 ? "00" : "30";
-                  const time = `${hour.toString().padStart(2, "0")}:${minute}`;
-
-                  return (
-                    <tr className="relative select-none h-5 " key={time}>
-                      <td className="absolute transform -translate-y-1/2 flex justify-end px-2 items-center text-sm w-16">
-                        {time}
-                      </td>
-                      {DAYS_OF_WEEK.map((day) => (
-                        <td
-                          key={`${day}-${time}`}
-                          className="h-[2.5rem] border border-gray-200"
-                        />
-                      ))}
-                    </tr>
-                  );
-                })}
-            </tbody>
-          </table>
-        </div>
-      )}
+      ) : <WeekCalendarSkeleton />}
     </>
   );
 };
